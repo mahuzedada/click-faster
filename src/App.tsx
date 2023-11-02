e3a7801f7e1dc0695e6c1c8749e7a1c2b525d801
Chatis Santos <chatis@afrointelligence.com>
2023-11-02 00:13:23 -0400

    Added a final report with some animation and transitions, a delayed restart button, and refactor timer logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
 import { useState, useEffect } from "react";
 
+const duration = 5;
+
 function App() {
-    const [time, setTime] = useState(3);
+    const [time, setTime] = useState(duration);
     const [clicks, setClicks] = useState(0);
+    const [showRestart, setShowRestart] = useState(false);
 
     function handleClick() {
         setClicks((current) => current + 1);
     }
 
     useEffect(() => {
-        if (time > 0) {
-            setTimeout(() => {
-                setTime(time - 1);
-            }, 1000);
+        if (time === 0) {
+            const timeoutId = setTimeout(() => setShowRestart(true), 3000);
+            return () => clearTimeout(timeoutId);
         }
+
+        const timerId = setTimeout(() => {
+            setTime(time - 1);
+        }, 1000);
+
+        return () => clearTimeout(timerId);
     }, [time]);
+
+    function resetGame() {
+        setTime(duration);
+        setClicks(0);
+        setShowRestart(false);
+    }
 
     return (
         <div className="App bg-gray-100 min-h-screen flex flex-col items-center justify-center p-4 text-center">
@@ -24,17 +38,36 @@ function App() {
             <p className="mb-8 text-lg text-gray-700 max-w-md">
                 Try to click the button as many times as you can before the timer reaches zero. Ready, set, go!
             </p>
-            <div className="text-4xl font-semibold mb-2">{time}</div>
-            <div className="text-3xl mb-8">Clicks: {clicks}</div>
-            <button
-                disabled={time <= 0}
-                onClick={handleClick}
-                className="disabled:opacity-50 text-white font-bold py-2 px-4 rounded bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 focus:outline-none focus:ring transition ease-in duration-200 text-lg"
-            >
-                Click me
-            </button>
+            {time > 0 ? (
+                <>
+                    <div className="text-4xl font-semibold mb-2">{time}</div>
+                    <div className="text-3xl mb-8">Clicks: {clicks}</div>
+                    <button
+                        disabled={time <= 0}
+                        onClick={handleClick}
+                        className="disabled:opacity-50 text-white font-bold py-2 px-4 rounded bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600 focus:outline-none focus:ring transition ease-in duration-200 text-lg"
+                    >
+                        Click me
+                    </button>
+                </>
+            ) : (
+                <>
+                    <div className="text-4xl font-semibold mb-2 animate-bounce">Time's up!</div>
+                    <div className="text-3xl font-bold mb-8 scale-150 transition duration-500 ease-in-out transform">
+                        Final Clicks: {clicks}
+                    </div>
+                    {showRestart && (
+                        <button
+                            onClick={resetGame}
+                            className="text-white font-bold py-2 px-4 rounded bg-gradient-to-r from-indigo-600 to-purple-700 hover:bg-gradient-to-l focus:outline-none focus:ring transition ease-in duration-200 text-lg"
+                        >
+                            Restart
+                        </button>
+                    )}
+                </>
+            )}
         </div>
     );
 }
 
-export default App
+export default App;
